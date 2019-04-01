a5a874eafb1efe7248c6342aca676a2245acb9eb
Oleg Grafov <olegraf@gmail.com>
2019-04-01 18:51:09 +0300

    add datastructure to load data

---
+++ ConsoleApp/MLDataSet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Decisions.ML
+{
+    public class MLDataSet
+    {
+        public MLFeature[] Features { get; set; }
+        public MLDataRow[] Rows { get; set; }
+    }
+
+    public class MLDataRow
+    {
+        public string ID { get; set; }
+        public Dictionary<string, object>[] Data { get; set; }
+    }
+
+    public class MLFeature
+    {
+        public string Name { get; set; }
+        public Type Type { get; set; }
+    }
+}
